cfec9a611b19c46a294c74812e7f10e028857e3e
Magnus <magnusmhp@gmail.com>
2019-06-08 18:32:09 +0200

    level stuff

---
+++ Tower Defence/Assets/LevelManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    int CurrentLevel;
+
+    public Transform spawnPoint;
+    public Transform[] waypoints;
+
+    List<GameObject> activeUnits = new List<GameObject>();
+
+    public GameObject[] enemies;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Unit.levelManager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(activeUnits.Count <= 0)
+        {
+            StartCoroutine(spawnUnits(CurrentLevel));
+
+        }
+
+    }
+
+    IEnumerator spawnUnits(int level)
+    {
+
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject unit = Instantiate(enemies[level], spawnPoint.position, Quaternion.identity);
+            unit.GetComponent<Unit>().waypoints = waypoints;
+
+            activeUnits.Add(unit);
+            yield return new WaitForSeconds(0.3f);
+        }
+        
+
+
+    }
+
+
+    public void removeFromActiveUnits(GameObject obj)
+    {
+        activeUnits.Remove(obj);
+    }
+}
--- Tower Defence/Assets/Unit.cs
+++ Tower Defence/Assets/Unit.cs
@@ -4,15 +4,82 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    public static LevelManager levelManager;
+
+    public Transform[] waypoints;
+    int currentWaypoint = 0;
+
+    public float speed = 5;
+    public float maxHealth = 100;
+    float health;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = maxHealth;
+        lookAtWaypoint();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        walkToWayPoint();
+
+
     }
+
+    void walkToWayPoint()
+    {
+
+
+
+        if(currentWaypoint < waypoints.Length)
+        {
+
+            transform.Translate(Vector3.forward *Time.deltaTime *speed);
+
+            Vector3 pos = waypoints[currentWaypoint].position;
+            pos.y = transform.position.y;
+
+            if (Vector3.Distance(transform.position, pos) < 0.1f)
+            {
+
+                
+                currentWaypoint += 1;
+
+                if(currentWaypoint >= waypoints.Length)
+                {
+                    levelManager.removeFromActiveUnits(gameObject);
+                    Destroy(gameObject);
+                }
+                else
+                lookAtWaypoint();
+            }
+
+        }
+
+
+
+        
+
+    }
+
+
+
+    
+    void lookAtWaypoint()
+    {
+        Vector3 pos = waypoints[currentWaypoint].position;
+        pos.y = transform.position.y;
+
+        transform.LookAt(pos);
+
+    }
+
+
 }
